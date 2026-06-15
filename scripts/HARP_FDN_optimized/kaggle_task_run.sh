1409bd75b988c8169c52ff3fba8a3d7b0968a4ab
25690 <17344736169@163.com>
2026-06-15 21:43:33 +0800

    xiufu

--- scripts/HARP_FDN_optimized/kaggle_task_run.sh
+++ scripts/HARP_FDN_optimized/kaggle_task_run.sh
@@ -16,9 +16,9 @@ echo "开始串行执行指定脚本"
 echo "============================================="
 
 # 手动按顺序执行，想跳过哪个就不写哪一行
-bash "${TASK_SCRIPTS_DIR}/search_ETTh2_336.sh"
-bash "${TASK_SCRIPTS_DIR}/search_ETTm1_720.sh"
-bash "${TASK_SCRIPTS_DIR}/search_ETTm2_96.sh"
+# bash "${TASK_SCRIPTS_DIR}/search_ETTh2_336.sh"
+# bash "${TASK_SCRIPTS_DIR}/search_ETTm1_720.sh"
+# bash "${TASK_SCRIPTS_DIR}/search_ETTm2_96.sh"
 bash "${TASK_SCRIPTS_DIR}/search_ETTm2_720.sh"
 
 echo -e "\n✅ 所有指定脚本执行完毕"
